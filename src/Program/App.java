568cfb7ad727de0a9b7acca2517733b17deea07d
AdrianGFO <adriangabriel.fo1@gmail.com>
2024-08-30 13:05:24 -0300

    first commit

---
+++ src/Program/App.java
@@ -0,0 +1,16 @@
+package Program;
+
+import entities.Worker;
+import entities.Enums.WorkerLevel;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        System.out.println("Enter worker data: ");
+        
+        Worker lucio = new Worker("Lucio", WorkerLevel.JUNIOR, 2570.0);
+    
+        
+    
+    }
+}
---
+++ src/entities/Worker.java
@@ -0,0 +1,52 @@
+package entities;
+
+import entities.Enums.WorkerLevel;
+import entities.HourContract;
+
+public class Worker {
+    private String name; 
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    public Worker(String name, WorkerLevel level, Double baseSalary){
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setLevel(WorkerLevel level){
+        this.level = level;
+    }
+
+    public WorkerLevel getLevel(){
+        return this.level;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void addContract(HourContract contract){
+        
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Worker [name=" + name + ", level=" + level + ", baseSalary=" + baseSalary + "]";
+    }
+    
+}
